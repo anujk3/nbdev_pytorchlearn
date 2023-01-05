1d55c495f8613cdb323cb38466e783ae0df417ac
Anuj Katiyal <anuj_katiyal@condenast.com>
2023-01-05 16:55:24 -0500

    back up

--- nbdev_pytorchlearn/_modidx.py
+++ nbdev_pytorchlearn/_modidx.py
@@ -5,4 +5,4 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://anujk3.github.io',
                 'git_url': 'https://github.com/anujk3/nbdev_pytorchlearn',
                 'lib_path': 'nbdev_pytorchlearn'},
-  'syms': {'nbdev_pytorchlearn.core': {'nbdev_pytorchlearn.core.foo': ('core.html#foo', 'nbdev_pytorchlearn/core.py')}}}
+  'syms': {'nbdev_pytorchlearn.core': {'nbdev_pytorchlearn.core.say_hello': ('core.html#say_hello', 'nbdev_pytorchlearn/core.py')}}}
